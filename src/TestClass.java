cdb99be247d324a19f508c56dc1c617af7d3f923
suheybsinan <117838600+suheybsinan@users.noreply.github.com>
2022-11-09 18:40:13 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,7 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git den merhaba!");
+        System.out.println("GitHub dan merhaba!");
 
     }
 }
